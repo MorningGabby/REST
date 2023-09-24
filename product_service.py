f853a25636d448f581c665750e2c437f3a22fd42
MorningGabby <112138090+MorningGabby@users.noreply.github.com>
2023-09-24 19:50:33 -0400

    Update product_service.py

--- product_service.py
+++ product_service.py
@@ -1,8 +1,10 @@
+#Gabriella Rivera
+#Assignment 2
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Hardcoded list of products
+#list of products
 products = [
     {'id': 1, 'name': 'Apple', 'price': 1.0, 'quantity': 100},
     {'id': 2, 'name': 'Banana', 'price': 0.5, 'quantity': 150},
@@ -11,8 +13,10 @@ products = [
 
 @app.route('/products', methods=['GET'])
 def get_products():
+    #return list
     return jsonify(products)
-
+    
+#find product with product id
 @app.route('/products/<int:product_id>', methods=['GET'])
 def get_product(product_id):
     product = next((product for product in products if product['id'] == product_id), None)
@@ -20,6 +24,7 @@ def get_product(product_id):
         return jsonify({'error': 'Product not found'}), 404
     return jsonify(product)
 
+#adds new product to list by extracting data from request
 @app.route('/products', methods=['POST'])
 def add_product():
     data = request.get_json()
