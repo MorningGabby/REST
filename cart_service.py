5df84a6cf6f493d068ce7949f22a3cbd8fd7225a
MorningGabby <112138090+MorningGabby@users.noreply.github.com>
2023-09-24 11:56:23 -0400

    Update cart_service.py

--- cart_service.py
+++ cart_service.py
@@ -3,11 +3,11 @@ import requests
 
 app = Flask(__name__)
 
-#storage for carts
+# Storage for carts
 carts = {}
 
-#URL of product service
-PRODUCT_SERVICE_URL = 'http://127.0.0.1:5000/products'
+# URL of the deployed Product Service on Render
+PRODUCT_SERVICE_URL = 'https://saas-assignment2-iw8y.onrender.com'
 
 @app.route('/cart/<int:user_id>', methods=['GET'])
 def get_cart(user_id):
@@ -15,13 +15,11 @@ def get_cart(user_id):
     total_price = sum(item['price'] * item['quantity'] for item in cart.values())
     return jsonify({'cart': list(cart.values()), 'total_price': total_price})
 
-PRODUCT_SERVICE_URL = 'https://saas-assignment2-iw8y.onrender.com'
-
 @app.route('/cart/<int:user_id>/add/<int:product_id>', methods=['POST'])
 def add_to_cart(user_id, product_id):
     quantity = request.json.get('quantity', 1)
     
-    response = requests.get(f'{PRODUCT_SERVICE_URL}/{product_id}')
+    response = requests.get(f'{PRODUCT_SERVICE_URL}/products/{product_id}')
     if response.status_code != 200:
         return jsonify({'message': 'Product not found'}), 404
 
@@ -34,7 +32,6 @@ def add_to_cart(user_id, product_id):
         cart[product_id] = {'id': product_id, 'name': product['name'], 'price': product['price'], 'quantity': quantity}
     
     return jsonify(cart[product_id])
-
 
 @app.route('/cart/<int:user_id>/remove/<int:product_id>', methods=['POST'])
 def remove_from_cart(user_id, product_id):
@@ -52,3 +49,4 @@ def remove_from_cart(user_id, product_id):
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
+
