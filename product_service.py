67ed83eadd19f09a4efe743eed2f0a81811702f4
MorningGabby <112138090+MorningGabby@users.noreply.github.com>
2023-09-24 19:39:23 -0400

    Update product_service.py

--- product_service.py
+++ product_service.py
@@ -1,45 +1,33 @@
 from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
-db = SQLAlchemy(app)
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
+# Hardcoded list of products
+products = [
+    {'id': 1, 'name': 'Apple', 'price': 1.0, 'quantity': 100},
+    {'id': 2, 'name': 'Banana', 'price': 0.5, 'quantity': 150},
+    {'id': 3, 'name': 'Orange', 'price': 0.8, 'quantity': 80},
+]
 
 @app.route('/products', methods=['GET'])
 def get_products():
-    products = Product.query.all()
-    return jsonify([product.serialize for product in products])
+    return jsonify(products)
 
 @app.route('/products/<int:product_id>', methods=['GET'])
 def get_product(product_id):
-    product = Product.query.get_or_404(product_id)
-    return jsonify(product.serialize)
+    product = next((product for product in products if product['id'] == product_id), None)
+    if product is None:
+        return jsonify({'error': 'Product not found'}), 404
+    return jsonify(product)
 
 @app.route('/products', methods=['POST'])
 def add_product():
     data = request.get_json()
-    new_product = Product(name=data['name'], price=data['price'], quantity=data['quantity'])
-    db.session.add(new_product)
-    db.session.commit()
-    return jsonify(new_product.serialize), 201
+    product_id = max(product['id'] for product in products) + 1
+    new_product = {'id': product_id, 'name': data['name'], 'price': data['price'], 'quantity': data['quantity']}
+    products.append(new_product)
+    return jsonify(new_product), 201
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        if Product.query.count() == 0:  #Check if the products table is empty
-            #Adding sample products
-            sample_products = [
-                {'name': 'Apple', 'price': 1.0, 'quantity': 100},
-                {'name': 'Banana', 'price': 0.5, 'quantity': 150},
-                {'name': 'Orange', 'price': 0.8, 'quantity': 80}
-            ]
-            for product in sample_products:
-                db.session.add(Product(**product))
-            db.session.commit()
     app.run(port=5000, debug=True)
+
